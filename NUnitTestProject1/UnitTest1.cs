e5daf125b863c3baacb4f8b147e02c2f887959d4
shuruq-alshahrani3 <85189223+shuruq-alshahrani3@users.noreply.github.com>
2021-10-06 07:45:02 +0300

    Update UnitTest1.cs

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.IO;
+using System.Reflection;
+using Assert = NUnit.Framework.Assert;
 
 namespace NUnitTestProject1
 {
@@ -27,7 +30,7 @@ namespace NUnitTestProject1
                 //  Assert.Equals("Index", _driver.Title);
                 //_driver.FindElement(By.XPath(@"/html/body/div[1]/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[1]/div[1]/div/input")).SendKeys("test");
                 // HttpClient http=
-                // Assert.Pass();
+                Assert.Pass();
 
             }
         
